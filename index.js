c7acb9b977dede06579de02a2d3bbc18bdbe8282
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2024-02-18 22:36:12 +0530

    basic chat

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.send("Hello bro");
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(3000, () => {
